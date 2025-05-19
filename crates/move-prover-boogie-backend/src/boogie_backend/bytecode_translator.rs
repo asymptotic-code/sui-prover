298272ccf2ff4369f9f9951c0fde958171d9cde5
Andrii <andrii@asymptotic.tech>
2025-05-19 20:28:17 +0300

    feat: added omit opaque feature

--- crates/move-prover-boogie-backend/src/boogie_backend/bytecode_translator.rs
+++ crates/move-prover-boogie-backend/src/boogie_backend/bytecode_translator.rs
@@ -434,6 +434,8 @@ impl<'env> BoogieTranslator<'env> {
         let mut builder =
             FunctionDataBuilder::new(spec_fun_target.func_env, spec_fun_target.data.clone());
         let code = std::mem::take(&mut builder.data.code);
+
+        let omit_havok = self.targets.omits_opaque(&spec_fun_target.func_env.get_qualified_id());
         for bc in code.into_iter() {
             match style {
                 FunctionTranslationStyle::Default => match bc {
@@ -485,6 +487,9 @@ impl<'env> BoogieTranslator<'env> {
                         let dests_clone = dests.clone();
                         let srcs_clone = srcs.clone();
                         builder.emit(bc.update_abort_action(|_| None));
+                        if omit_havok {
+                            continue;
+                        }
                         let callee_fun_env = self.env.get_function(module_id.qualified(fun_id));
                         for (ret_idx, temp_idx) in dests_clone.iter().enumerate() {
                             let havoc_kind = if callee_fun_env
@@ -530,6 +535,9 @@ impl<'env> BoogieTranslator<'env> {
                         let dests_clone = dests.clone();
                         let srcs_clone = srcs.clone();
                         builder.emit(bc);
+                        if omit_havok {
+                            continue;
+                        }
                         let callee_fun_env = self.env.get_function(module_id.qualified(fun_id));
                         for (ret_idx, temp_idx) in dests_clone.iter().enumerate() {
                             let havoc_kind = if callee_fun_env
@@ -2321,10 +2329,12 @@ impl<'env> FunctionTranslator<'env> {
                                 FunctionTranslationStyle::Default,
                             );
 
+                            let id = &self.fun_target.func_env.get_qualified_id();
+
                             if self
                                 .parent
                                 .targets
-                                .get_fun_by_spec(&self.fun_target.func_env.get_qualified_id())
+                                .get_fun_by_spec(id)
                                 == Some(&QualifiedId {
                                     module_id: *mid,
                                     id: *fid,
@@ -2340,7 +2350,10 @@ impl<'env> FunctionTranslator<'env> {
                                 } else if self.style == FunctionTranslationStyle::SpecNoAbortCheck
                                     || self.style == FunctionTranslationStyle::Opaque
                                 {
-                                    fun_name = format!("{}{}", fun_name, "$opaque");
+                                    let use_impl = self.parent.targets.omits_opaque(id);
+                                    let verified = self.parent.targets.is_verified_spec(id);
+                                    let suffix = if use_impl { if verified { "$impl" } else { "" } } else { "$opaque" };
+                                    fun_name = format!("{}{}", fun_name, suffix);
                                 }
                             };
 
@@ -2473,23 +2486,27 @@ impl<'env> FunctionTranslator<'env> {
                             }
                         }
 
+                        let id = &self.fun_target.func_env.get_qualified_id();
+
                         if self
                             .parent
                             .targets
-                            .get_fun_by_spec(&self.fun_target.func_env.get_qualified_id())
+                            .get_fun_by_spec(id)
                             == Some(&mid.qualified(*fid))
                             && (self.style == FunctionTranslationStyle::SpecNoAbortCheck
                                 || self.style == FunctionTranslationStyle::Opaque)
                         {
-                            for type_inst in
-                                spec_global_variable_analysis::get_info(&self.fun_target.data)
-                                    .mut_vars()
-                            {
-                                emitln!(
-                                    self.writer(),
-                                    "havoc {};",
-                                    boogie_spec_global_var_name(self.parent.env, type_inst),
-                                );
+                            if !self.parent.targets.omits_opaque(id) {
+                                for type_inst in
+                                    spec_global_variable_analysis::get_info(&self.fun_target.data)
+                                        .mut_vars()
+                                {
+                                    emitln!(
+                                        self.writer(),
+                                        "havoc {};",
+                                        boogie_spec_global_var_name(self.parent.env, type_inst),
+                                    );
+                                }
                             }
                         };
 
