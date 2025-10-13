590c3c772ff9000de90315aae6476be5bd8dce11
Andrii <andrii@asymptotic.tech>
2025-10-13 18:34:12 +0300

    wip

--- crates/sui-prover/tests/sui-prover.sh
+++ crates/sui-prover/tests/sui-prover.sh
@@ -26,7 +26,8 @@ git clone --branch master https://github.com/boogie-org/boogie.git boogie-src
 cd boogie-src
 dotnet build Source/Boogie.sln -c Release
 cp -r Source/BoogieDriver/bin/Release/net8.0/* "$INSTALL_DIR"
-ln -s "$INSTALL_DIR/BoogieDriver" "$BIN_DIR/boogie"
+cp -r "$INSTALL_DIR/BoogieDriver" "$BIN_DIR/boogie"
+chmod -R +x "$BIN_DIR/boogie"
 cd ..
 rm -rf boogie-src
 
