397820bd96a87419c91ef5d5f05d9069d8d4cd35
Andrii <andrii@asymptotic.tech>
2025-10-16 19:28:24 +0300

    feat: main flow change

--- crates/move-model/src/code_writer.rs
+++ crates/move-model/src/code_writer.rs
@@ -8,7 +8,7 @@
 //!   so its on the bottom of the dependency relation, and there is no `utility` crate
 //!   where it could belong to.
 
-use std::{collections::BTreeMap, ops::Bound};
+use std::{collections::BTreeMap, fmt::Debug, ops::Bound};
 
 use codespan::{ByteIndex, ByteOffset, ColumnIndex, Files, LineIndex, RawIndex, RawOffset};
 
@@ -37,6 +37,20 @@ struct CodeWriterData {
     label_map: BTreeMap<ByteIndex, ByteIndex>,
 }
 
+impl Clone for CodeWriterData {
+    fn clone(&self) -> Self {
+        Self {
+            emit_hook: Box::new(|_| None), // NOTE: Cannot clone a closure, so use default. Hook is not used
+            output: self.output.clone(),
+            indent: self.indent,
+            current_location: self.current_location.clone(),
+            output_location_map: self.output_location_map.clone(),
+            label_map: self.label_map.clone(),
+        }
+    }
+}
+
+#[derive(Clone)]
 /// A helper to emit code. Supports indentation and maintains source to target location information.
 pub struct CodeWriter(RefCell<CodeWriterData>);
 
