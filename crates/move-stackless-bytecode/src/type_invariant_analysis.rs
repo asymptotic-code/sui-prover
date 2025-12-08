ddc97c815e474451de3711179b11780c26cc5a6d
Andrii <andrii@asymptotic.tech>
2025-12-08 19:11:42 +0200

    feat: removed deadcode

--- crates/move-stackless-bytecode/src/type_invariant_analysis.rs
+++ crates/move-stackless-bytecode/src/type_invariant_analysis.rs
@@ -50,6 +50,7 @@ fn analyze_type_invariants(
     results
 }
 
+// NOTE: we don't care about type cycles here, as they are resticted by Move type system/compiler
 fn analyze_type_invariants_r(
     targets: &FunctionTargetsHolder,
     env: &GlobalEnv,
@@ -150,102 +151,6 @@ fn process_type_inv_with_ensures(
     });
 }
 
-fn detect_recursion_cycles(targets: &FunctionTargetsHolder, env: &GlobalEnv, ty: &Type) -> bool {
-    let mut cycles = BTreeSet::new();
-    let mut visited = BTreeSet::new();
-    let mut path = Vec::new();
-
-    find_recursion_cycles_r(
-        env,
-        ty.skip_reference(),
-        &mut visited,
-        &mut path,
-        &mut cycles,
-    );
-
-    for cycle_ty in &cycles {
-        if let Type::Datatype(mid, datatype_id, _) = cycle_ty {
-            let qid = mid.qualified(*datatype_id);
-            if targets.get_inv_by_datatype(&qid).is_none() {
-                continue;
-            }
-
-            let loc = match env.get_struct_or_enum_qid(qid) {
-                StructOrEnumEnv::Struct(struct_env) => struct_env.get_loc(),
-                StructOrEnumEnv::Enum(enum_env) => enum_env.get_loc(),
-            };
-            env.diag(
-                Severity::Error,
-                &loc,
-                "Type invariant is not allowed for recursive types",
-            );
-            return true;
-        }
-    }
-
-    false
-}
-
-fn find_recursion_cycles_r(
-    env: &GlobalEnv,
-    ty: &Type,
-    visited: &mut BTreeSet<Type>,
-    path: &mut Vec<Type>,
-    cycles: &mut BTreeSet<Type>,
-) {
-    let ty = ty.skip_reference();
-
-    match ty {
-        Type::Datatype(mid, datatype_id, type_params) => {
-            let base_ty = Type::Datatype(*mid, *datatype_id, vec![]);
-
-            if let Some(cycle_start) = path.iter().position(|t| {
-                if let Type::Datatype(m, d, _) = t {
-                    *m == *mid && *d == *datatype_id
-                } else {
-                    false
-                }
-            }) {
-                // Found a cycle - add all types in the cycle
-                for cycle_ty in &path[cycle_start..] {
-                    cycles.insert(cycle_ty.clone());
-                }
-                cycles.insert(ty.clone());
-                return;
-            }
-
-            if visited.contains(&base_ty) {
-                return;
-            }
-
-            path.push(ty.clone());
-
-            let qid = mid.qualified(*datatype_id);
-            let fields: Vec<Type> = match env.get_struct_or_enum_qid(qid) {
-                StructOrEnumEnv::Struct(struct_env) => struct_env
-                    .get_fields()
-                    .map(|f| f.get_type().instantiate(type_params))
-                    .collect(),
-                StructOrEnumEnv::Enum(enum_env) => enum_env
-                    .get_all_fields()
-                    .map(|f| f.get_type().instantiate(type_params))
-                    .collect(),
-            };
-
-            for field_ty in fields {
-                find_recursion_cycles_r(env, &field_ty, visited, path, cycles);
-            }
-
-            path.pop();
-            visited.insert(base_ty);
-        }
-        Type::Vector(inner_ty) => {
-            find_recursion_cycles_r(env, inner_ty, visited, path, cycles);
-        }
-        _ => {}
-    }
-}
-
 impl FunctionTargetProcessor for TypeInvariantAnalysisProcessor {
     fn process(
         &self,
@@ -264,14 +169,6 @@ impl FunctionTargetProcessor for TypeInvariantAnalysisProcessor {
 
         builder.set_loc(builder.fun_env.get_loc().at_start());
         for param in 0..builder.fun_env.get_parameter_count() {
-            if detect_recursion_cycles(
-                &targets,
-                builder.global_env(),
-                &builder.get_local_type(param),
-            ) {
-                return builder.data;
-            }
-
             process_type_inv_with_requires(&mut builder, targets, param);
         }
 
