0a30615eb39f799003c80e9830fd07266ad8d79d
msaaltink <msaaltink@users.noreply.github.com>
2025-12-11 15:23:12 -0500

    Tests to confirm that code loops that compute quantifiers can be proved (#397)
    * Added tests to confirm that code loop that compute quantifiers (all, any, ...)
    can be proven (if a suitable invariabnt is supplied).
    
    Needed to tweak the axioms for count_range to make the proofs work.
    
    * fmt
    
    ---------
    
    Co-authored-by: andrii-a8c <andrii@asymptotic.tech>

--- crates/move-prover-boogie-backend/src/boogie_backend/bytecode_translator.rs
+++ crates/move-prover-boogie-backend/src/boogie_backend/bytecode_translator.rs
@@ -4857,9 +4857,19 @@ impl<'env> FunctionTranslator<'env> {
                             }
                             QuantifierType::CountRange => {
                                 emitln!(self.writer(), "havoc $quantifier_temp_vec;");
-                                emitln!(self.writer(), "assume $t{} <= $t{} ==> LenVec($quantifier_temp_vec) == ($t{} - $t{});", srcs[1], srcs[2], srcs[2], srcs[1]);
-                                emitln!(self.writer(), "assume (forall i:int :: $t{} <= i && i < $t{} ==> ReadVec($quantifier_temp_vec, i - $t{}) == (if {}({}) then 1 else 0));", srcs[1], srcs[2], srcs[1], fun_name, cr_args("i"));
-                                emitln!(self.writer(), "$t{} := $0_vec_$sum'u64'($quantifier_temp_vec, 0, LenVec($quantifier_temp_vec));", dests[0]);
+                                emitln!(
+                                    self.writer(),
+                                    "assume LenVec($quantifier_temp_vec) == LenVec($t{});",
+                                    srcs[0]
+                                );
+                                emitln!(self.writer(), "assume (forall i:int :: 0 <= i && i < LenVec($quantifier_temp_vec) ==> ReadVec($quantifier_temp_vec, i) == (if {}({}) then 1 else 0));", fun_name, cr_args("i"));
+                                emitln!(
+                                    self.writer(),
+                                    "$t{} := $0_vec_$sum'u64'($quantifier_temp_vec, $t{}, $t{});",
+                                    dests[0],
+                                    srcs[1],
+                                    srcs[2]
+                                );
                             }
                             QuantifierType::SumMap => {
                                 emitln!(self.writer(), "havoc $quantifier_temp_vec;");
