d29e6e3c9c2324255c22f23bddc709986c5dc106
Andrii <andrii@asymptotic.tech>
2025-12-22 20:36:15 +0200

    feat: introduced helper

--- crates/move-prover-boogie-backend/src/boogie_backend/bytecode_translator.rs
+++ crates/move-prover-boogie-backend/src/boogie_backend/bytecode_translator.rs
@@ -3147,13 +3147,10 @@ impl<'env> FunctionTranslator<'env> {
                 )
             }
             QuantifierType::Find => {
-                let src_type = if let Type::Vector(inner) =
-                    self.fun_target.get_local_type(srcs[0]).skip_reference()
-                {
-                    inner.as_ref()
-                } else {
-                    panic!("Expected vector type for quantifier operation")
-                };
+                let src_type = self
+                    .fun_target
+                    .get_local_type(srcs[0])
+                    .vector_element_type();
                 let src_elem_suffix = boogie_type_suffix(env, src_type);
                 let find_quant_name = self
                     .parent
@@ -3164,13 +3161,10 @@ impl<'env> FunctionTranslator<'env> {
                 )
             }
             QuantifierType::FindRange => {
-                let src_type = if let Type::Vector(inner) =
-                    self.fun_target.get_local_type(srcs[0]).skip_reference()
-                {
-                    inner.as_ref()
-                } else {
-                    panic!("Expected vector type for quantifier operation")
-                };
+                let src_type = self
+                    .fun_target
+                    .get_local_type(srcs[0])
+                    .vector_element_type();
                 let src_elem_suffix = boogie_type_suffix(env, src_type);
                 let find_quant_name = self
                     .parent
